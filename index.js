9cc8489433515597dc5eabbaac1e964db6b5c40e
KiruiTito <titokirui402@gmail.com>
2023-03-30 14:35:12 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+// multiply function
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
 
-// Write your code here
+// random function
+const random = Math.floor(Math.random() * 100) + 1;
+
+// mod function
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4;
+
+// max function
+const max = Math.max(5, 10, 15, 20);
